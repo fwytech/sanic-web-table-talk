d1e48d843193ebad9e1f3a0da682846cc4de67e6
fwytech <fwytech@126.com>
2025-11-17 16:43:31 +0800

    Add files via upload

---
+++ serv.py
@@ -0,0 +1,35 @@
+import os
+
+from sanic import Sanic
+from sanic.response import empty
+
+import controllers
+from common.route_utility import autodiscover
+from config.load_env import load_env
+
+# 加载配置文件
+load_env()
+
+app = Sanic("sanic-web")
+autodiscover(
+    app,
+    controllers,
+    recursive=True,
+)
+
+
+app.route("/")(lambda _: empty())
+
+
+def get_server_config():
+    """获取服务器配置参数"""
+    return {
+        "host": os.getenv("SERVER_HOST", "0.0.0.0"),
+        "port": int(os.getenv("SERVER_PORT", 8088)),
+        "workers": int(os.getenv("SERVER_WORKERS", 2)),
+    }
+
+
+if __name__ == "__main__":
+    config = get_server_config()
+    app.run(**config)
