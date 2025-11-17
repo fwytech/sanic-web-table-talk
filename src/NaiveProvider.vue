bab811e4ddf16d7c74bfda56894169cf2eb90992
fwytech <fwytech@126.com>
2025-11-17 17:38:49 +0800

    添加Naive UI提供器组件 src/NaiveProvider.vue

---
+++ src/NaiveProvider.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { createApp, defineComponent, h } from 'vue'
+import { useMessage, useNotification, useDialog, useLoadingBar } from 'naive-ui'
+
+function registerNaiveTools() {
+  window.$ModalMessage = useMessage()
+  window.$ModalNotification = useNotification()
+  window.$ModalDialog = useDialog()
+  window.$ModalLoadingBar = useLoadingBar()
+}
+
+const NaiveProviderWrapper = defineComponent({
+  name: 'NaiveProviderWrapper',
+  setup() {
+    registerNaiveTools()
+  },
+  render() {
+    return h('div')
+  },
+})
+</script>
+
+<template>
+  <NLoadingBarProvider>
+    <NDialogProvider>
+      <NNotificationProvider>
+        <NMessageProvider>
+          <slot></slot>
+          <NaiveProviderWrapper />
+        </NMessageProvider>
+      </NNotificationProvider>
+    </NDialogProvider>
+  </NLoadingBarProvider>
+</template>
